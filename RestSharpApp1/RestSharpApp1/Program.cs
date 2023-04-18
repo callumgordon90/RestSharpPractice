018204140acc2b4cad4e8eb4ee78117ba8ade5e7
cgordon <callumjosephiaingordon@gmail.com>
2023-04-18 17:02:15 +0200

    created a URL variable which contans the API url and then pass this variable as a parameter to a 'Client' variable

--- RestSharpApp1/RestSharpApp1/Program.cs
+++ RestSharpApp1/RestSharpApp1/Program.cs
@@ -7,10 +7,19 @@ using RestSharp;
 
 namespace RestSharpApp1
 {
+    //program class of the whole solution
     class Program
     {
+        //Main class where the code is executed
         static void Main(string[] args)
         {
+            //create a string variable to hold the url of the fake API:
+            string url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+            //make a rest client:
+            //this client can take the url of the API as a parameter
+            var client = new RestClient(url);
+
 
         }
     }
