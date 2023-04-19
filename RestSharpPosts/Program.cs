82fc53daf7ed3b0cead604607c6fbff61a0fb29c
cgordon <callumjosephiaingordon@gmail.com>
2023-04-19 16:11:09 +0200

    set up the basic endpoint url, the client and the request (all defined as objects)

--- RestSharpPosts/Program.cs
+++ RestSharpPosts/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI;
 
 namespace RestSharpPosts
 {
@@ -10,6 +12,21 @@ namespace RestSharpPosts
     {
         static void Main(string[] args)
         {
+
+            //This is the endpoint, saved in an object called 'url'.
+            string url = "https://jsonplaceholder.typicode.com/posts";
+
+            //The client is the entity which launches the request to the external API. Here the object 'client' is an instance of the
+            //class 'RestClient', and takes the destination enpoint (url) as a parameter to the method.
+            var client = new RestClient (url);
+
+            //The object 'request' is an instance of the 'RestRequest' class.
+            var request = new RestRequest ();
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+
         }
     }
 }
