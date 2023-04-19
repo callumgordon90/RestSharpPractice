6714e9929078c61f584481650eb907a0dce02029
cgordon <callumjosephiaingordon@gmail.com>
2023-04-19 16:24:40 +0200

    improved annotation

--- RestSharpPosts/post.cs
+++ RestSharpPosts/post.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace RestSharpPosts
 {
-    internal class post
+    //We are going to add 3 different properties to the class 'post'
+    //These are the 3 parts of the body that we are going to send with the POST request
+    public class post
     {
-    }
+        public int userId { get; set; }
+
+        public string body { get; set; }
+
+        public string title { get; set; }
+
+        //Now we can create an object from this ('post') class, and put in the values that we want for these three attributes,
+        //and convert it to JSON and send it with our request 
 }
